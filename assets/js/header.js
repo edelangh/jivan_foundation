b57f551fa607332c24f84c032f75aa563d4771ad
acorbeau <acorbeau@seed-up.io>
2018-02-25 19:13:58 +0100

    Salut

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         if ($(window).scrollTop() >= 130)
             $(".header").css({
                 'background-color': "#f0eac9",
-                'box-shadow': "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)"
+                'box-shadow': "0 1px 2px rgba(0,0,0,0.12), 0 1px 1px rgba(0,0,0,0.24)"
             });
         else 
             $(".header").css({
