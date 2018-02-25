fee83e13d68dd7384449f7cc5e9f9a5f064553c5
edelangh <edelangh@student.42.fr>
2018-02-25 19:04:54 +0100

    add text for language

--- languages/en.php
+++ languages/en.php
@@ -3,5 +3,10 @@ $LANG = array(
     "title" => "",
     "title_what_we_do" => "WHAT WE DO",
     "title_who_we_are" => "WHO WE ARE",
-    "title_get_involved" => "GET INVOLVED"
+    "title_get_involved" => "GET INVOLVED",
+    "quotes_title" => "A child is a child",
+    "quotes_text" =>
+"Children have not asked to be born in a slum, to leprosy parents or in a cast of rag pickers.
+A child has not chosen to do degrading or humiliating jobs, and hence should have equal rights to succeed in life, to be loved and to be happy. Education is a powerful way of achieving it",
+    "quotes_foot" => "Give back children their dignity.",
 );
