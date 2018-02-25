f416c17c39014d278841a662a9197e9580377bb7
acorbeau <acorbeau@seed-up.io>
2018-02-25 18:02:05 +0100

    Header sub menu

--- languages/en.php
+++ languages/en.php
@@ -3,5 +3,9 @@ $LANG = array(
     "title" => "",
     "title_what_we_do" => "WHAT WE DO",
     "title_who_we_are" => "WHO WE ARE",
+    "title_who_sub_what" => "What is Jivan",
+    "title_who_sub_story" => "Jivan's story",
+    "title_who_sub_behind" => "The people behind",
+    "title_who_sub_assoc" => "The association",
     "title_get_involved" => "GET INVOLVED"
 );
