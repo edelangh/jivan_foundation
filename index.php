87c5488e81afcc11a80be34eb01847db85bda4ec
acorbeau <acorbeau@seed-up.io>
2018-02-25 15:51:26 +0100

    Base template

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+            include 'head.php';
+        ?>
+    </head>
+    <body>
+        <header>
+            <?php
+                include 'header.php';
+            ?>
+        </header>
+        <!--   -->
+
+
+        <!--   -->
+        <?php
+            include 'foot.php';
+        ?>
+        <footer>
+            <?php
+                include 'footer.php';
+            ?>
+        </footer>
+    </body>
+</html>
