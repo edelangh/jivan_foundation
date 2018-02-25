12f1e2371c7b9127b1da4c09ddc740d7d041fcba
acorbeau <acorbeau@seed-up.io>
2018-02-25 16:00:35 +0100

    Yolo

--- index.php
+++ index.php
@@ -1,26 +1,40 @@
+<?php
+    $PAGES = array(
+        "404" => "./page_404.php",
+        "index" => "./page_index.php",
+    )
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <?php
-            include 'head.php';
+            include './head.php';
         ?>
     </head>
     <body>
         <header>
             <?php
-                include 'header.php';
+                include './header.php';
             ?>
         </header>
         <!--   -->
-
-
+        <?php
+            if (isset($_GET["page"])) {
+                if (isset($PAGES[$_GET["pages"]]))
+                    require $PAGES[$_GET["pages"]];
+                else
+                    require $PAGES["404"];    
+            }
+            else
+                require $PAGES["index"];
+        ?>
         <!--   -->
         <?php
-            include 'foot.php';
+            include './foot.php';
         ?>
         <footer>
             <?php
-                include 'footer.php';
+                include './footer.php';
             ?>
         </footer>
     </body>
