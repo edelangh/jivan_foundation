097a8afef4821555bdd6b55964baf72c81dd7523
edelangh <edelangh@student.42.fr>
2018-02-25 17:46:01 +0100

    add paypal button

--- header.php
+++ header.php
@@ -5,5 +5,10 @@
         <img alt="Brand" >
       </a>
     </div>
+    <div class="navbar-header">
+      <a class="navbar-brand" href="?page=donate">
+        <img alt="donate" src="assets/imgs/donate.png">
+      </a>
+    </div>
   </div>
 </nav>
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     $PAGES = array(
         "404" => "./page_404.php",
         "index" => "./page_index.php",
+        "donate" => "./page_donate.php",
     )
 ?>
 <!DOCTYPE html>
@@ -21,8 +22,8 @@
             <!--   -->
             <?php
                 if (isset($_GET["page"])) {
-                    if (isset($PAGES[$_GET["pages"]]))
-                        require $PAGES[$_GET["pages"]];
+                    if (isset($PAGES[$_GET["page"]]))
+                        require $PAGES[$_GET["page"]];
                     else
                         require $PAGES["404"];    
                 }
