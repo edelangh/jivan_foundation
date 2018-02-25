a6635529e11e24e006422c30c0a923b1f8a6d5f0
acorbeau <acorbeau@seed-up.io>
2018-02-25 16:35:51 +0100

    Salut

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <div class="navbar-header">
       <a class="navbar-brand" href="#">
-        <img alt="Brand" >
+        <img id="main-logo" alt="Jivan" src="./assets/imgs/logo-jivan.png"/>
       </a>
     </div>
   </div>
