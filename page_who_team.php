fb092a93813598f5e476195bb7b8399b66ed6ffa
edelangh <edelangh@student.42.fr>
2018-03-02 12:41:45 +0100

    change title colors ffccb3 to ffbb80

--- page_who_team.php
+++ page_who_team.php
@@ -6,7 +6,7 @@
     <div class="row body-content-part quotes">
         <div class="col-sm-0 col-md-1"></div>
         <div style="text-align: left" class="col-sm-12 col-md-10">
-            <h1 style="color:#ffccb3"><?php echo $TITLE; ?></h1>
+            <h1><?php echo $TITLE; ?></h1>
             <hr align="center" width="90%">
             <div style="font-size: 1.3em">
                 <h4>Anita BULA</h5>
