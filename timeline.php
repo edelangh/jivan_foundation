365ed266a2a484dcb5df6247cce89adda051e4b0
edelangh <edelangh@student.42.fr>
2018-03-13 23:49:41 +0100

    fix asya shit

--- timeline.php
+++ timeline.php
@@ -1,8 +1,7 @@
 <!-- http://freefrontend.com/css-timelines/ -->
 <div class="timeline-container" id="timeline-1">
   <div class="timeline">
-<?php $i = 3;
-   while ($i) { $i -= 1; ?>
+   <?php $i = 4; while ($i) { $i -= 1; ?>
     <div class="timeline-item" data-text="FATHER OF THE TURKS">
       <div class="timeline__content"><img class="timeline__img" src="http://i.cdn.ensonhaber.com/resimler/diger/ataturk_3473.jpg"/>
         <h2 class="timeline__content-title">1881</h2>
