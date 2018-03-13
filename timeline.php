da35d6bf842e1f7c29e016f438787b6c381fa87e
edelangh <edelangh@student.42.fr>
2018-03-13 23:43:05 +0100

    add timeline files

---
+++ timeline.php
@@ -0,0 +1,15 @@
+<!-- http://freefrontend.com/css-timelines/ -->
+<div class="timeline-container" id="timeline-1">
+  <div class="timeline">
+<?php $i = 3;
+   while ($i) { $i -= 1; ?>
+    <div class="timeline-item" data-text="FATHER OF THE TURKS">
+      <div class="timeline__content"><img class="timeline__img" src="http://i.cdn.ensonhaber.com/resimler/diger/ataturk_3473.jpg"/>
+        <h2 class="timeline__content-title">1881</h2>
+        <p class="timeline__content-desc">He was born in 1881 (probably in the spring) in Salonica, then an Ottoman city, now inGreece. His father Ali Riza, a customs official turned lumber merchant, died when Mustafawas still a boy. His mother Zubeyde, adevout and strong-willed woman, raised him and his sister.</p>
+      </div>
+    </div>
+    <?php } ?>
+  </div>
+</div>
+<script src="./assets/js/timeline.js" type="text/javascript"></script>
