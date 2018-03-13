3bb5fc4f148d9f5cddabd4dfd2416e5ba11ff025
acorbeau <acorbeau@seed-up.io>
2018-03-13 21:59:29 +0100

    Salut

--- page_gi_touch.php
+++ page_gi_touch.php
@@ -1,13 +1,21 @@
 
-<div class="container body-content">
+<!--div class="container body-content">
     <div class="row body-content-part quotes">
         <div class="col-sm-0 col-md-1"></div>
         <div style="text-align: left" class="col-sm-12 col-md-10">
             <hr align="center" width="90%">
             <div style="font-size: 1.2em; text-align:center;">
-		<a href="http://tse-foundation.org/contact/"><?php echo $LANG["gi_contact"]; ?></a>
-		<p style="text-align: center"><?php echo $LANG["gi_touch"]; ?></p>
+		<a href="http://tse-foundation.org/contact/">< ?php echo $LANG["gi_contact"]; ?></a>
+		<p style="text-align: center">< ?php echo $LANG["gi_touch"]; ?></p>
             </div>
         </div>
+    </div>
+</div-->
+<div class="container body-content row">
+    <div class="col-sm-12">
+        <input id="mail-mail-area" type="email" placeholder="Your email address"></input>
+    </div>
+    <div class="col-sm-12">
+        <textarea id="mail-text-area"></textarea>
     </div>
 </div>
