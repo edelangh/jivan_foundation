07f2015259ec3448bba5b351d16f45be6935469c
edelangh <edelangh@student.42.fr>
2018-03-20 00:41:12 +0100

    edit donate

--- page_gi_donate.php
+++ page_gi_donate.php
@@ -1,2 +1,12 @@
 
-<iframe id="haWidget" src="https://www.donnerenligne.fr/jivan-test/faire-un-don" style="padding-top:3em; width:100%;height:750px;border:none;" onload="window.scroll(0, this.offsetTop)"></iframe><div style="width:100%;text-align:center;">Propulsé par <a href="https://www.helloasso.com" rel="nofollow">HelloAsso</a></div>
+<div class="body-content">
+  <div class="container onepage" style="padding-top: 0vh">
+    <iframe id="haWidget" src="https://www.donnerenligne.fr/jivan-test/faire-un-don"
+      style="width:100%;height:80vh;border:none;"
+      onload="window.scroll(0, this.offsetTop)">
+    </iframe>
+    <div style="width:100%;text-align:center;">
+      Propulsé par <a href="https://www.helloasso.com" rel="nofollow">HelloAsso</a>
+    </div>
+  </div>
+</div>
