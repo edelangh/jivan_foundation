ec8a754bff7abfe9f8a9e298d44675dca1d57419
acorbeau <acorbeau@seed-up.io>
2018-03-22 22:22:46 +0100

    Fix what we do

--- page_gi_we_do.php
+++ page_gi_we_do.php
@@ -2,15 +2,18 @@
 <div class="body-content">
 <div class="container onepage">
     <div class="row quotes">
-      <div class="col-sm-6">
-        <h2 style="text-transform: uppercase;padding-top: 1em;" >
+    <div class="col-sm-12">
+        <h2 style="text-transform: uppercase;" >
         WHAT WE DO
 
-        </h2>
+</h2>
+
+  </div>
+      <div class="col-sm-6">
         <p><?php echo $LANG["who_what_body_part_1"]; ?></p>
         <p><?php echo $LANG["who_what_body_part_2"]; ?></p>
       </div>
-      <div style="padding-top: 1em;" class="col-sm-6 img-show">
+      <div class="col-sm-6 img-show">
         <img style="margin: 15px; float: right;" class="img-sepia main-img" src="assets/imgs/slide3.jpg"/>
       </div>
     </div>
