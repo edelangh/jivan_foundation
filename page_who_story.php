4ae9e1ad1a801793ac1d4f5eb87c764caf8e1002
acorbeau <acorbeau@seed-up.io>
2018-03-22 22:19:21 +0100

    Uniform

--- page_who_story.php
+++ page_who_story.php
@@ -7,9 +7,10 @@
   <div class="container onepage">
     <div class="row quotes">
         <div style="text-align: left" class="col-sm-12 col-md-12">
-          <h2><?php echo $TITLE; ?></h2>
-          <div class="sidebar-box sidebar-long">
-            <p class="description" style="padding-left: 10vw; padding-right: 10vw; padding-bottom: 10vw">
+          <div style="padding-bottom: 10vw" class="sidebar-box sidebar-long">
+            <p class="description">
+            <h2><?php echo $TITLE; ?></h2>          
+
               <?php echo $BODY; ?>
             </p>
             <p class="read-more"><a href="#" class="btn btn-donate btn-outline-success button">Read More</a></p>
