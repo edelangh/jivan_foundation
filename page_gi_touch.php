38196dba64221dcfb32d71fb3d83c6a76a521641
edelangh <edelangh@student.42.fr>
2018-03-23 00:37:37 +0100

    add images

--- page_gi_touch.php
+++ page_gi_touch.php
@@ -4,8 +4,8 @@
     <div class="row quotes">
       <div class="col-sm-8" style="text-align: center;">
         <h3 style="padding-bottom: 1em; text-align: left;">Leave a message</h3>
-        <input id="mail-mail-area" type="email" placeholder="Your email address"></input>
-        <textarea id="mail-text-area" placeholder="Your message"></textarea>
+        <input id="mail-mail-area" type="email" placeholder="Your email address" class="letter-bg"></input>
+        <textarea id="mail-text-area" placeholder="Your message" class="letter-bg"></textarea>
         <button class="btn btn-donate btn-outline-success my-2 my-sm-0" type="submit">Send</button>
       </div>
       <div style="color: #98838f;" class="col-sm-4">
