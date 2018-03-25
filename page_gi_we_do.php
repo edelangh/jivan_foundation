232bcb2ef004afbde0e73198d6150db21e0ef567
edelangh <edelangh@student.42.fr>
2018-03-25 21:25:30 +0200

    asd

--- page_gi_we_do.php
+++ page_gi_we_do.php
@@ -1,21 +1,16 @@
 
 <div class="body-content">
-<div class="container onepage">
-    <div class="row quotes">
-    <div class="col-sm-12">
-        <h2 style="text-transform: uppercase;" >
-        WHAT WE DO
-
-</h2>
-
-  </div>
-      <div class="col-sm-6">
-        <p><?php echo $LANG["who_what_body_part_1"]; ?></p>
-        <p><?php echo $LANG["who_what_body_part_2"]; ?></p>
-      </div>
-      <div class="col-sm-6 img-show">
-        <img style="margin: 15px; float: right;" class="img-sepia main-img" src="assets/imgs/slide3.jpg"/>
-      </div>
+    <div class="container onepage">
+     <div class="row quotes">
+     <div class="col-sm-12">
     </div>
-</div>
+    <div class="col-sm-6">
+      <p><?php echo $LANG["who_what_body_part_1"]; ?></p>
+      <p><?php echo $LANG["who_what_body_part_2"]; ?></p>
+    </div>
+    <div class="col-sm-6 img-show">
+      <img style="margin: 15px; float: right;" class="img-sepia main-img" src="assets/imgs/slide3.jpg"/>
+    </div>
+    </div>
+  </div>
 </div>
