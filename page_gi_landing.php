94f970de3118f51be42c866eac22c28ff54ad922
acorbeau <acorbeau@seed-up.io>
2018-03-25 20:19:46 +0200

    d

--- page_gi_landing.php
+++ page_gi_landing.php
@@ -1,19 +1,28 @@
 
-<div class="body-content">
+<div style="max-width: 60vw; margin-left: calc(15vw - 1em);"  class="body-content">
   <div style="padding-top: 5vh;" class="container onepage">
     <div class="row quotes">
         <div style="text-align: left;" class="index-param col-sm-12 col-md-6">
-            <h2><?php echo $LANG["gi_title_1"]; ?></h2>
+            <h2>
+                <?php echo $LANG["gi_title_1"]; ?>
+            </h2>
             <p style="text-align: left">
                 <?php echo $LANG["gi_text_1"]; ?>
             </p>
-            <h3><a href="?page=gi_donate">
-                <?php echo $LANG["gi_bottom_1"]; ?>
-            </h3></a>
+            <h3>
+                <a href="?page=gi_donate">
+                    <?php echo $LANG["gi_bottom_1"]; ?>
+                </a>
+            </h3>
+        </div>
+        <div width="100%" class="col-sm-0 col-md-6">
+            <img style="padding-top: 1em;  float: right;"src="assets/imgs/img_puz.png"></img>
         </div>
     </div>
     <div class="row quotes">
-        <div class="col-sm-0 col-md-6"></div>
+    <div width="100%" class="col-sm-0 col-md-6">
+            <img style="padding-top: 1em; float: left;"src="assets/imgs/img_hand.png"></img>
+        </div>
         <div style="text-align: right;" class="index-param col-sm-12 col-md-6">
             <h2><?php echo $LANG["gi_title_2"]; ?></h2>
             <p style="text-align: right">
@@ -21,7 +30,7 @@
             </p>
             <h3><a href="?page=gi_ambassador">
                 <?php echo $LANG["gi_bottom_2"]; ?>
-            </h3></a>
+                </a></h3>
         </div>
     </div>
     <div class="row quotes">
@@ -30,11 +39,16 @@
             <p style="text-align: left">
                 <?php echo $LANG["gi_text_3"]; ?>
             </p>
-            <h3><a href="?page=gi_work">
+            <h3>
+            <a href="?page=gi_work">
                 <?php echo $LANG["gi_bottom_3"]; ?>
-            </h3></a>
+            </a>
+            </h3>
+        </div>
+        <div width="100%" class="col-sm-0 col-md-6">
+            <img style="padding-top: 1em; float: right;"src="assets/imgs/wave.png"></img>
+        </div>
 	</div>
-     </div>
   </div>
 </div>
 
--- page_gi_touch.php
+++ page_gi_touch.php
@@ -3,10 +3,13 @@
   <div class="container onepage">
     <div class="row quotes">
       <div class="col-sm-8" style="text-align: center;">
-        <h3 style="padding-bottom: 1em; text-align: left;">Leave a message</h3>
-        <input id="mail-mail-area" type="email" placeholder="Your email address"></input>
-        <textarea id="mail-text-area" placeholder="Your message"></textarea>
+        <form action="#">
+        <h3 style="text-align: left;">Dear jivan foundation,</h3>
+        <textarea  style="margin-top: 0px; "id="mail-text-area" placeholder="Your message" autofocus></textarea>
+        <input id="mail-mail-area" type="email" placeholder="Your email address" ></input>
+        <p></p>
         <button class="btn btn-donate btn-outline-success my-2 my-sm-0" type="submit">Send</button>
+        </form>
       </div>
       <div style="color: #98838f;" class="col-sm-4">
         <h3 style="padding-bottom: 1em"><?php echo $LANG["gi_contact"]; ?></h3>
