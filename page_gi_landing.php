e9d0753ff4856716265e49464797ac5e26b7ea21
edelangh <edelangh@student.42.fr>
2018-04-09 14:42:42 +0200

    cc

--- page_gi_landing.php
+++ page_gi_landing.php
@@ -1,16 +1,16 @@
 
 <div class="body-content" style="padding-bottom: 30px">
-  <div class="container onepage">
-    <div class="row quotes">
+  <div class="container">
+    <div class="row quotes onepage landing_one">
         <div class="index-param col-sm-12 col-md-6">
             <img src="assets/imgs/wind.png"></img>
             <h2><?php echo $LANG["gi_title_1"]; ?></h2>
             <p><?php echo $LANG["gi_text_1"]; ?></p>
 	</div>
-        <div class="col-sm-12 d-md-none" style="display: contents">
-          <b style="margin: auto"><a class="blink" href="?page=gi_donate">
+        <div class="col-sm-12 d-md-none" style="text-align: center">
+          <a class="blink" href="?page=gi_donate">
              <?php echo $LANG["gi_bottom_1"]; ?>
-          </a></b>
+          </a>
         </div>
         <div class="d-md-none mob-spacer"></div>
         <div class="index-param col-sm-12 col-md-6">
@@ -18,23 +18,23 @@
             <h2><?php echo $LANG["gi_title_4"]; ?></h2>
             <p><?php echo $LANG["gi_text_4"]; ?></p>
         </div>
-        <div class="col-sm-12" style="display: contents">
-          <b style="margin: auto"><a class="blink" href="?page=gi_donate">
+        <div class="col-sm-12" style="text-align: center">
+          <a class="blink" href="?page=gi_donate">
              <?php echo $LANG["gi_bottom_1"]; ?>
-          </a></b>
+          </a>
         </div>
         <div class="d-md-none mob-spacer"></div>
     </div>
-    <div class="row quotes">
+    <div class="row quotes onepage">
         <div class="index-param col-sm-12 col-md-6">
             <img src="assets/imgs/wave.png"></img>
             <h2><?php echo $LANG["gi_title_2"]; ?></h2>
             <p><?php echo $LANG["gi_text_2"]; ?></p>
 	</div>
-        <div class="col-sm-12 d-md-none" style="display: contents">
-          <b style="margin: auto"><a class="blink" href="?page=gi_touch">
+        <div class="col-sm-12 d-md-none" style="text-align: center">
+          <a class="blink" href="?page=gi_touch">
              <?php echo $LANG["gi_bottom_2"]; ?>
-          </a></b>
+          </a>
         </div>
         <div class="d-md-none mob-spacer"></div>
         <div class="index-param col-sm-12 col-md-6">
@@ -42,10 +42,10 @@
             <h2><?php echo $LANG["gi_title_3"]; ?></h2>
             <p><?php echo $LANG["gi_text_3"]; ?></p>
         </div>
-        <div class="col-sm-12" style="display: contents">
-          <b style="margin: auto"><a class="blink" href="?page=gi_touch">
+        <div class="col-sm-12" style="text-align: center">
+          <a class="blink" href="?page=gi_touch">
              <?php echo $LANG["gi_bottom_2"]; ?>
-          </a></b>
+          </a>
         </div>
         <div class="d-md-none mob-spacer"></div>
     </div>
