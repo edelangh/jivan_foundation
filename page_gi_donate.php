8beb4fe15b2aad89412fa91734355b44fa228c05
edelangh <edelangh@student.42.fr>
2018-04-10 21:00:32 +0200

    add en for donate

--- page_gi_donate.php
+++ page_gi_donate.php
@@ -1,2 +1,6 @@
 <div id="wtf" style="background-color: #f6f3e1;position:absolute; width:100%; height:7.2em; top:0; left: 0"></div>
-<iframe id="haWidget" allowtransparency="true" src="https://www.donnerenligne.fr/jivan/faire-un-don"style="height: 100%;padding-top: -10em;width:100%;border:none;" onload="window.scroll(0, this.offsetTop)"></iframe>
+<?PHP if ($LL == "en") { ?>
+<iframe id="haWidget" allowtransparency="true" src="https://www.helloasso.com/associations/jivan/formulaires/1/widget/en" style="height: 100%;padding-top: -10em;width:100%;border:none;" onload="window.scroll(0, this.offsetTop)"></iframe>
+<?PHP } else { ?>
+<iframe id="haWidget" allowtransparency="true" src="https://www.donnerenligne.fr/jivan/faire-un-don" style="height: 100%;padding-top: -10em;width:100%;border:none;" onload="window.scroll(0, this.offsetTop)"></iframe>
+<?PHP } ?>
